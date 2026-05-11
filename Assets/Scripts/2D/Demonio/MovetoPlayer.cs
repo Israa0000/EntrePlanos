5770a06914b852495e827ffd93392a857dcb1519
AZF <titomonguer@gmail.com>
2026-05-11 17:46:26 +0200

    simetria stage 1 y stage
    cambio de escena 2d a 3d

--- Assets/Scripts/2D/Demonio/MovetoPlayer.cs
+++ Assets/Scripts/2D/Demonio/MovetoPlayer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MovetoPlayer : MonoBehaviour
 {
@@ -32,6 +33,7 @@ public class MovetoPlayer : MonoBehaviour
         if(collision.gameObject.CompareTag("Player"))
         {
         print("Player hit");
+            SceneManager.LoadScene(4);
         }
     }
 
